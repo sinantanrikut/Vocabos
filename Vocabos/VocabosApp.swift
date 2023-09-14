ae986f8d25c0c711d01631be5c626efbc826e5ca
sinanecodation <122370642+sinanecodation@users.noreply.github.com>
2023-09-14 15:14:55 +0300

    Initial Commit

---
+++ Vocabos/VocabosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabosApp.swift
+//  Vocabos
+//
+//  Created by Sinan Tanrıkut on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct VocabosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
